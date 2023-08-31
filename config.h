183b539a1837bf41e5ae6e9f9da0d8cd099785db
Hannah Morrison <hannah@landhmorrison.scot>
2023-08-31 10:56:01 +0100

    First attempt at using side keys only for layers

--- config.h
+++ config.h
@@ -65,3 +65,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Enable key repeat - disabled for now
 // #define QUICK_TAP_TERM 100
+
+// Define the tap-hold tap dance action.
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
+    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
