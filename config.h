4ac8cca9aa623f5d7cff3f8be550789e9dd6e99d
Hannah Morrison <hannah@landhmorrison.scot>
2023-09-21 12:15:22 +0100

    Make simpler version of thumbkeys layout

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Enable kinetic mouse
-//#define MK_KINETIC_SPEED
+#define MK_KINETIC_SPEED
 
 // Set the mouse settings to a comfortable speed/accuracy trade-off, assuming a
 // screen refresh rate of 60 Htz or higher The default is 50. This makes the
@@ -28,30 +28,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // The default is 20. Since we made the mouse about 3 times faster with the
 // previous setting, give it more time to accelerate to max speed to retain
 // precise control over short distances.
-//#define MOUSEKEY_TIME_TO_MAX 120
-#define MOUSEKEY_TIME_TO_MAX 64
+#define MOUSEKEY_TIME_TO_MAX 120
 #define MOUSEKEY_MAX_SPEED 6
 
 // The default is 300. Let's try and make this as low as possible while keeping
 // the cursor responsive
-//#define MOUSEKEY_DELAY 100
-#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_DELAY 100
 
 // It makes sense to use the same delay for the mouseweel
-//#define MOUSEKEY_WHEEL_DELAY 100
-#define MOUSEKEY_WHEEL_DELAY 0
+#define MOUSEKEY_WHEEL_DELAY 100
 
 // The default is 100
-//#define MOUSEKEY_WHEEL_INTERVAL 50
+#define MOUSEKEY_WHEEL_INTERVAL 50
 
 // The default is 40
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 64
 
 // Vaguely tuned settings for kinetic mouse wheel
-//#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 12
-//#define MOUSEKEY_WHEEL_BASE_MOVEMENTS 32
-//#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 32
-//#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 12
+#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 12
+#define MOUSEKEY_WHEEL_BASE_MOVEMENTS 32
+#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 32
+#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 12
 
 // Default tapping term
 #define TAPPING_TERM 190
@@ -60,10 +57,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Tapping term for the homerow mod keys
 #define TAPPING_TERM_MODS 700
-
-// Tapping term for tap dance keys
-#define TAPPING_TERM_TD_THUMB 160
-
-// Define the tap-hold tap dance action.
-#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
-    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
--- keymap.c
+++ keymap.c
@@ -2,12 +2,10 @@
 
 enum my_layers {
   BASE_LAYER,
-  SHIFT_LAYER,
-  SPACE_LAYER,
-  ENTER_LAYER,
-  ESCAPE_LAYER,
-  LEFT_LAYER,
-  RIGHT_LAYER
+  NAV_LAYER,
+  NUM_LAYER,
+  SCUT_LAYER,
+  SYM_LAYER
 };
 
 enum my_keycodes {
@@ -30,63 +28,6 @@ enum my_keycodes {
     M_ISWIN
 };
 
-// Tap-hold tap dances require custom types and functions.
-
-typedef enum {
-  TD_NONE,
-  TD_UNKNOWN,
-  TD_SINGLE_TAP,
-  TD_DOUBLE_TAP,
-  TD_HOLD
-} td_state_t;
-
-typedef struct {
-  td_state_t state;
-} td_tap_t;
-
-static td_tap_t td_shift = {
-  .state = TD_NONE
-};
-
-static td_tap_t td_space = {
-  .state = TD_NONE
-};
-
-static td_tap_t td_enter = {
-  .state = TD_NONE
-};
-
-static td_tap_t td_escape = {
-  .state = TD_NONE
-};
-
-td_state_t td_get_state(tap_dance_state_t *state);
-
-void shift_finished(tap_dance_state_t *state, void *user_data);
-void shift_reset(tap_dance_state_t *state, void *user_data);
-void space_finished(tap_dance_state_t *state, void *user_data);
-void space_reset(tap_dance_state_t *state, void *user_data);
-void enter_finished(tap_dance_state_t *state, void *user_data);
-void enter_reset(tap_dance_state_t *state, void *user_data);
-void escape_finished(tap_dance_state_t *state, void *user_data);
-void escape_reset(tap_dance_state_t *state, void *user_data);
-
-// Tap Dance definitions
-
-enum {
-  TD_SHIFT,
-  TD_SPACE,
-  TD_ENTER,
-  TD_ESCAPE
-};
-
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_SHIFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, shift_finished, shift_reset),
-    [TD_SPACE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, space_finished, space_reset),
-    [TD_ENTER] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, enter_finished, enter_reset),
-    [TD_ESCAPE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, escape_finished, escape_reset)
-};
-
 // Stores state of M_ALTT macro - true if we are currently tabbing between
 // windows.
 static bool m_altt_pressed = false;
@@ -95,55 +36,47 @@ static bool m_altt_pressed = false;
 // function layer
 static bool m_is_chromebook = false;
 
+// Used to temporarily store the state of the mod keys.
+static uint8_t mod_state = 0;
+
+// State for managing shift backspace behaviour.
+static bool kc_del_registered = false;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE_LAYER] = LAYOUT_split_3x5_2(
-    LSFT_T(KC_Q),     LCTL_T(KC_W),  LALT_T(KC_F),  LGUI_T(KC_P),  KC_B,  KC_J,  LGUI_T(KC_L),  LALT_T(KC_U),  LCTL_T(KC_Y),  KC_BSPC,
-    KC_A,             KC_R,          KC_S,          KC_T,          KC_G,  KC_M,  KC_N,          KC_E,          KC_I,          KC_O,
-    OSL(LEFT_LAYER),  KC_X,          KC_C,          KC_D,          KC_V,  KC_K,  KC_H,          KC_COMM,       KC_DOT,        OSL(RIGHT_LAYER),
-    TD(TD_SHIFT),     TD(TD_SPACE),  TD(TD_ENTER),  TD(TD_ESCAPE)
+    KC_Q,           LCTL_T(KC_W),  LALT_T(KC_F),  LGUI_T(KC_P),  KC_B,     KC_J,           LGUI_T(KC_L),  LALT_T(KC_U),  LCTL_T(KC_Y),  KC_BSPC,
+    KC_A,           KC_R,          KC_S,          KC_T,          KC_G,     KC_M,           KC_N,          KC_E,          KC_I,          KC_O,
+    KC_Z,           KC_X,          KC_C,          KC_D,          KC_V,     KC_K,           KC_H,          KC_COMM,       KC_DOT,        OSL(SCUT_LAYER),
+    OSM(MOD_LSFT),  LT(NAV_LAYER, KC_SPC),       LT(NUM_LAYER, KC_ENT),  OSL(SYM_LAYER)
   ),
 
-  [SHIFT_LAYER] = LAYOUT_split_3x5_2(
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_NO,    KC_MS_U,  KC_NO,    KC_DEL,
-    KC_NO,    KC_ACL0,  KC_ACL1,  KC_ACL2,  KC_NO,  KC_WH_U,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_WH_D,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_TRNS,  KC_TRNS,  KC_BTN1,  KC_BTN2
-  ),
-
-  [SPACE_LAYER] = LAYOUT_split_3x5_2(
-    KC_NO,   KC_PSCR,  M_ISCB,   M_ISWIN,  KC_INS,   M_XTAB,   M_PDESK,  LCTL(KC_TAB),  M_ALTT,   M_NDESK,
-    KC_NO,   KC_MNXT,  KC_MPLY,  KC_VOLU,  KC_BRIU,  KC_NO,    KC_LEFT,  KC_DOWN,       KC_UP,    KC_RGHT,
-    KC_CAPS,  KC_MPRV,  KC_MUTE,  KC_VOLD,  KC_BRID,  KC_NO,    KC_HOME,  KC_PGDN,       KC_PGUP,  KC_END,
+  [NAV_LAYER] = LAYOUT_split_3x5_2(
+    KC_NO,    KC_PSCR,  M_ISCB,   M_ISWIN,  KC_INS,   M_XTAB,  M_PDESK,  LCTL(KC_TAB),  M_ALTT,   M_NDESK,
+    KC_NO,    KC_MNXT,  KC_MPLY,  KC_VOLU,  KC_BRIU,  KC_NO,   KC_LEFT,  KC_DOWN,       KC_UP,    KC_RGHT,
+    KC_CAPS,  KC_MPRV,  KC_MUTE,  KC_VOLD,  KC_BRID,  KC_NO,   KC_HOME,  KC_PGDN,       KC_PGUP,  KC_END,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-  [ENTER_LAYER] = LAYOUT_split_3x5_2(
-    KC_PAST,  KC_1,     KC_2,     KC_3,    KC_PPLS,  KC_NO,  KC_F1,   KC_F2,    KC_F3,  KC_NO,
-    KC_PSLS,  KC_4,     KC_5,     KC_6,    KC_PMNS,  KC_NO,  KC_F4,   KC_F5,    KC_F6,  KC_NO,
-    KC_0,     KC_7,     KC_8,     KC_9,    KC_DOT,   KC_NO,  KC_F7,   KC_F8,    KC_F9,  KC_F10,
+  [NUM_LAYER] = LAYOUT_split_3x5_2(
+    KC_PAST,  KC_1,     KC_2,     KC_3,    KC_PPLS,  KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_NO,
+    KC_PSLS,  KC_4,     KC_5,     KC_6,    KC_PMNS,  KC_NO,  KC_F4,  KC_F5,  KC_F6,  KC_NO,
+    KC_0,     KC_7,     KC_8,     KC_9,    KC_DOT,   KC_NO,  KC_F7,  KC_F8,  KC_F9,  KC_F10,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-  [ESCAPE_LAYER] = LAYOUT_split_3x5_2(
+  [SCUT_LAYER] = LAYOUT_split_3x5_2(
     M_ESCQ,   M_ESCW,      M_NTRM,      M_1PASS,           M_LIKE,      KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,
-    KC_NO,    M_APP1,      M_APP2,      M_APP3,            M_APP4,      KC_NO,  KC_NO,  M_EMOJI,  KC_NO,  KC_NO,
-    KC_NO,    LCTL(KC_X),  LCTL(KC_C),  LSFT(LCTL(KC_C)),  LCTL(KC_V),  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,
+    KC_ESC,   M_APP1,      M_APP2,      M_APP3,            M_APP4,      KC_NO,  KC_NO,  M_EMOJI,  KC_NO,  KC_NO,
+    KC_CAPS,  LCTL(KC_X),  LCTL(KC_C),  LSFT(LCTL(KC_C)),  LCTL(KC_V),  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,
     KC_TRNS,  KC_TRNS,     KC_TRNS,     KC_TRNS
   ),
 
-  [LEFT_LAYER] = LAYOUT_split_3x5_2(
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_CIRC,  KC_AMPR,        KC_ASTR,        KC_UNDS,  KC_PLUS,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_COLN,  LSFT(KC_QUOT),  LSFT(KC_BSLS),  KC_MINS,  KC_EQL,
-    KC_Z,     KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_SCLN,  KC_QUOT,        KC_BSLS,        KC_GRV,   KC_EXLM,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
-  ),
-
-  [RIGHT_LAYER] = LAYOUT_split_3x5_2(
-    KC_ESC,   LSFT(KC_2),     LSFT(KC_3),  KC_DLR,   KC_PERC,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_TAB,   LSFT(KC_NUBS),  KC_LBRC,     KC_LCBR,  KC_LPRN,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    CW_TOGG,  KC_NUBS,        KC_RBRC,     KC_RCBR,  KC_RPRN,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_SLSH,
-    KC_TRNS,  KC_TRNS,        KC_TRNS,     KC_TRNS
+  [SYM_LAYER] = LAYOUT_split_3x5_2(
+    KC_EXCL,         LSFT(KC_2),     LSFT(KC_3),  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,        KC_ASTR,        KC_UNDS,  KC_PLUS,
+    KC_TAB,          LSFT(KC_NUBS),  KC_LBRC,     KC_LCBR,  KC_LPRN,  KC_COLN,  LSFT(KC_QUOT),  LSFT(KC_BSLS),  KC_MINS,  KC_EQL,
+    CW_TOGG,         KC_NUBS,        KC_RBRC,     KC_RCBR,  KC_RPRN,  KC_SCLN,  KC_QUOT,        KC_BSLS,        KC_GRV,   KC_SLSH,
+    TO(BASE_LAYER),  KC_TAB,         KC_TRNS,     KC_ESC
   ),
 
 };
@@ -157,21 +90,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     m_altt_pressed = false;
   }
 
-  // Ensure shift is not pressed when the left and right layers is active.
-  if (IS_LAYER_ON(LEFT_LAYER) || IS_LAYER_ON(RIGHT_LAYER)) {
-    switch (keycode) {
-      case OSL(LEFT_LAYER):
-      case OSL(RIGHT_LAYER):
-      case KC_Z:
-      case KC_SLSH:
-        break;
-      default:
-        del_mods(MOD_MASK_SHIFT);
-        del_oneshot_mods(MOD_MASK_SHIFT);
-    }
-  }
+  mod_state = get_mods();
 
   switch (keycode) {
+    // Shift-backspace produces delete.
+    case KC_BSPC:
+      if (record->event.pressed) {
+        if (mod_state & MOD_MASK_SHIFT) {
+          del_mods(MOD_MASK_SHIFT);
+          register_code(KC_DEL);
+          kc_del_registered = true;
+          set_mods(mod_state);
+          return false;
+        }
+      } else {
+        if (kc_del_registered) {
+          unregister_code(KC_DEL);
+          kc_del_registered = false;
+          return false;
+        }
+      }
+      break;
     case M_ESCQ:
       if (record->event.pressed) {
         SEND_STRING(SS_TAP(X_ESC)":q!"SS_TAP(X_ENT));
@@ -365,7 +304,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     // Set the tapping term for the homerow mods.
-    case LSFT_T(KC_Q):
     case LCTL_T(KC_W):
     case LALT_T(KC_F):
     case LGUI_T(KC_P):
@@ -373,167 +311,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case LALT_T(KC_U):
     case LCTL_T(KC_Y):
       return TAPPING_TERM_MODS;
-    // Set the tapping term for tap dance keys.
-    case TD(TD_SHIFT):
-    case TD(TD_SPACE):
-    case TD(TD_ENTER):
-    case TD(TD_ESCAPE):
-      return TAPPING_TERM_TD_THUMB;
     default:
       return TAPPING_TERM;
   }
-}
-
-td_state_t td_get_state(tap_dance_state_t *state) {
-  if (state->count == 1) {
-    // If the keypress has been interrupted by another keypress or is no longer
-    // held down by the end of the tap time, then we know it was just a single
-    // tap.
-    if (state->interrupted || !state->pressed)
-      return TD_SINGLE_TAP;
-    else
-      // If it is still held at the end of the tap time, then it is a hold.
-      return TD_HOLD;
-  } else if (state->count > 1) {
-    // In the case where there has been more than one tap in the tap time, we
-    // have to make a judgement call. If the key is still held down we will
-    // assume it was a flutter and call it a hold.
-    if (state->pressed)
-      return TD_HOLD;
-    // If the keypress has finished or has been interrupted then we will assume
-    // that there was some actual fast typing going on and issue two taps, which
-    // will be the most common scenario.
-    if (state->interrupted || !state->pressed)
-      return TD_DOUBLE_TAP;
-  }
-  return TD_UNKNOWN;
-}
-
-void shift_finished(tap_dance_state_t *state, void *user_data) {
-    td_shift.state = td_get_state(state);
-    switch (td_shift.state) {
-        case TD_SINGLE_TAP:
-          set_oneshot_mods(MOD_LSFT);
-          break;
-        case TD_DOUBLE_TAP:
-          caps_word_on();
-          break;
-        case TD_HOLD:
-          layer_on(SHIFT_LAYER);
-          break;
-        default:
-          break;
-    }
-}
-
-void shift_reset(tap_dance_state_t *state, void *user_data) {
-    switch (td_shift.state) {
-        case TD_SINGLE_TAP:
-          // Do nothing - the oneshot mod will switch itself off after another
-          // key has been pressed.
-          break;
-        case TD_DOUBLE_TAP:
-          // Do nothing - caps word will finish itself.
-          break;
-        case TD_HOLD:
-          layer_off(SHIFT_LAYER);
-        default:
-          break;
-    }
-    td_shift.state = TD_NONE;
-}
-
-void space_finished(tap_dance_state_t *state, void *user_data) {
-    td_space.state = td_get_state(state);
-    switch (td_space.state) {
-        case TD_SINGLE_TAP:
-          register_code(KC_SPC);
-          break;
-        case TD_DOUBLE_TAP:
-          register_code(KC_TAB);
-          break;
-        case TD_HOLD:
-          layer_on(SPACE_LAYER);
-          break;
-        default:
-          break;
-    }
-}
-
-void space_reset(tap_dance_state_t *state, void *user_data) {
-    switch (td_space.state) {
-        case TD_SINGLE_TAP:
-          unregister_code(KC_SPC);
-          break;
-        case TD_DOUBLE_TAP:
-          unregister_code(KC_TAB);
-          break;
-        case TD_HOLD:
-          layer_off(SPACE_LAYER);
-        default:
-          break;
-    }
-    td_space.state = TD_NONE;
-}
-
-void enter_finished(tap_dance_state_t *state, void *user_data) {
-    td_enter.state = td_get_state(state);
-    switch (td_enter.state) {
-        case TD_DOUBLE_TAP:
-          register_code(KC_ENT);
-          unregister_code(KC_ENT);
-        case TD_SINGLE_TAP:
-          register_code(KC_ENT);
-          break;
-        case TD_HOLD:
-          layer_on(ENTER_LAYER);
-          break;
-        default:
-          break;
-    }
-}
-
-void enter_reset(tap_dance_state_t *state, void *user_data) {
-    switch (td_enter.state) {
-        case TD_DOUBLE_TAP:
-        case TD_SINGLE_TAP:
-          unregister_code(KC_ENT);
-          break;
-        case TD_HOLD:
-          layer_off(ENTER_LAYER);
-        default:
-          break;
-    }
-    td_enter.state = TD_NONE;
-}
-
-void escape_finished(tap_dance_state_t *state, void *user_data) {
-    td_escape.state = td_get_state(state);
-    switch (td_escape.state) {
-        case TD_DOUBLE_TAP:
-          register_code(KC_ESC);
-          unregister_code(KC_ESC);
-        case TD_SINGLE_TAP:
-          register_code(KC_ESC);
-          break;
-        case TD_HOLD:
-          layer_on(ESCAPE_LAYER);
-          break;
-        default:
-          break;
-    }
-}
-
-void escape_reset(tap_dance_state_t *state, void *user_data) {
-    switch (td_escape.state) {
-        case TD_DOUBLE_TAP:
-        case TD_SINGLE_TAP:
-          unregister_code(KC_ESC);
-          break;
-        case TD_HOLD:
-          layer_off(ESCAPE_LAYER);
-        default:
-          break;
-    }
-    td_escape.state = TD_NONE;
 }
